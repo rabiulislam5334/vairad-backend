c4bd1f08b2ee54ee03c85ae0bf9d34d2165913cc
rabiulislam5334 <rabiulislam5334@gmail.com>
2026-07-04 13:39:44 +0600

    accounts/views.py updated and accounts/serializers.py created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'full_name', 'is_staff', 'is_active')
+    ordering = ('email',)
+    search_fields = ('email', 'full_name')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+
+
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = 'email'
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['user'] = UserSerializer(self.user).data
+        return data
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = EmailTokenObtainPairSerializer
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
